3a29f777e4fd6a857f4821fac6e438ce99dc7163
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2022-01-15 16:18:36 -0500

    Add weather config items to application config

--- src/config/application-config.ts
+++ src/config/application-config.ts
@@ -2,6 +2,11 @@ import * as dotenv from 'dotenv';
 
 dotenv.config()
 
+export interface Location {
+    lat: number,
+    long: number
+};
+
 export class ApplicationConfig {
 
     getGoogleOAuthClientId(): string {
@@ -12,4 +17,14 @@ export class ApplicationConfig {
         return process.env.GOOGLE_OAUTH_CLIENT_SECRET;
     }
 
+    getOpenWeatherApiToken(): string {
+        return process.env.OPEN_WEATHER_API_KEY;
+    }
+
+    getLocation(): Location {
+        return {
+            lat: parseFloat(process.env.WEATHER_LOCATION_LATITUDE),
+            long: parseFloat(process.env.WEATHER_LOCATION_LONGITUDE),
+        }
+    }
 }
